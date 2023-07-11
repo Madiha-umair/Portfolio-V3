3a6f44cf47aa6a96fad809c20a99f4371a1d1e29
Madiha Umair <madiha.altaf@yahoo.com>
2023-07-10 21:17:15 -0400

    blog edited

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,35 +1,44 @@
 import { useState, useEffect } from "react";
 
 export default function Blogs() {
-    const [blog, setBlog] = useState([]);
+  const [blog, setBlog] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(null);
 
-    useEffect(() => {
-        const getBlog = async () => {
-          let response = await fetch("./laravel-blade-cms.json");
-          let databaseTables = await response.json();
-          //console.log(databaseTables);
+  useEffect(() => {
+    const getBlog = async () => {
+      let response = await fetch("./laravel-blade-cms.json");
+      let databaseTables = await response.json();
 
-          let each_blog = databaseTables[2].data.map((item) => ({
-            title: item.title,
-            blog: item.blog
-          }));
-          setBlog(each_blog);
-        };
-        getBlog();
-      }, []);
+      let each_blog = databaseTables[2].data.map((item) => ({
+        title: item.title,
+        blog: item.blog
+      }));
+      setBlog(each_blog);
+    };
+    getBlog();
+  }, []);
 
-    return (
-        <div id="blog_container">
-        <h2>Blogs</h2>
+  const handleClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  return (
+    <div >
+      <h2>Blogs</h2>
+      <div className="blog-container">
         <ul>
           {blog.map((element, index) => (
-            <li key={index}>
-             <h2> {element.title}</h2>
-             <p> {element.blog}</p>
-           </li>
+            <li
+              key={index}
+              className={activeIndex === index ? "active" : ""}
+              onClick={() => handleClick(index)}
+            >
+              <h2>{element.title}</h2>
+              <p>{element.blog}</p>
+            </li>
           ))}
-          <hr id="line" />
         </ul>
       </div>
-    );
-  }
+    </div>
+  );
+}
