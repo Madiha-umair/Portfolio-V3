396c3ee2b724ebc667dc6aeebf4c06bbf087aeed
Madiha Umair <madiha.altaf@yahoo.com>
2023-07-10 21:00:30 -0400

    experience css

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,42 +1,45 @@
 import { useState, useEffect } from "react";
 
 export default function Experience() {
-    const [experience, setExperience] = useState([]);
+  const [experience, setExperience] = useState([]);
 
-    useEffect(() => {
-        const getExperience = async () => {
-          let response = await fetch("./laravel-blade-cms.json");
-          let databaseTables = await response.json();
-          //console.log(databaseTables);
+  useEffect(() => {
+    const getExperience = async () => {
+      let response = await fetch("./laravel-blade-cms.json");
+      let databaseTables = await response.json();
 
-          let each_experience = databaseTables[3].data.map((item) => ({
-            title:item.title,
-            company:item.company,
-            start:item.start,
-            end:item.end,
-            location:item.location,
-            detail:item.detail
-          }));
-          setExperience(each_experience);
-        };
-        getExperience();
-      }, []);
+      let each_experience = databaseTables[3].data.map((item) => ({
+        title: item.title,
+        company: item.company,
+        start: item.start,
+        end: item.end,
+        location: item.location,
+        detail: item.detail,
+      }));
+      setExperience(each_experience);
+    };
+    getExperience();
+  }, []);
 
-    return (
-        <div id="experience_container">
-        <h2>Experience</h2>
-        <ul>
-          {experience.map((entity, index) => (
-            <li key={index}>
-             <h3> {entity.title}</h3>
-             <h4> {entity.company}</h4>
-             <p> {entity.location}</p>
-             <p> {entity.start} - {entity.end}</p>
-             <p> {entity.detail}</p>
-           </li>
-          ))}
-          <hr id="line" />
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Experience</h2>
+    <div id="experience_container">
+      <ul>
+        {experience.map((entity, index) => (
+          <li key={index}>
+            <h3>{entity.title}</h3>
+            <h4>{entity.company}</h4>
+            <p>{entity.location}</p>
+            <p>
+              {entity.start} - {entity.end}
+            </p>
+            <p>{entity.detail}</p>
+          </li>
+        ))}
+        <hr id="line" />
+      </ul>
+    </div>
+    </div>
+  );
+}
